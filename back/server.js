08301b7e8cee382a7a56b8852a5b3715abad01f7
Aidar <aidarionov@gmail.com>
2022-04-29 13:18:51 +0600

    add workout controller

--- back/server.js
+++ back/server.js
@@ -7,6 +7,8 @@ import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 /* Routes */
 import userRoutes from './routes/userRoutes.js';
+import exerciseRoutes from './routes/exerciseRoutes.js'
+import workoutRoutes from './routes/workoutRoutes.js'
 
 const app = express();
 dotenv.config();
@@ -19,8 +21,10 @@ if(process.env.NODE_ENV === 'development') {
 
 app.use(express.json());
 
-
 app.use('/api/users', userRoutes); 
+app.use('/api/exercises', exerciseRoutes); 
+app.use('/api/workouts', workoutRoutes); 
+
 app.use(notFound);
 app.use(errorHandler);
 
